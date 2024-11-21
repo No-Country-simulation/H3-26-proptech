596a2983e3fc9441ccd360314f9616f3fe82c07b
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2024-11-21 01:16:38 -0300

    Add: Install Shadcn, start of site footer component

---
+++ frontend/src/components/site-footer.tsx
@@ -0,0 +1,18 @@
+import { siteConfig } from "@/config/site";
+import Link from "next/link";
+
+export function SiteFooter() {
+  return (
+    <footer className="flex h-36 w-full border-t border-slate-200 p-4">
+      <div className="flex items-start gap-2">
+        <div className="">
+          Suscríbete
+          <p className="text-sm">
+            Recibe las últimas oportunidades de inversión en terrenos,
+            actualizaciones y consejos exclusivos directamente en tu correo.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
